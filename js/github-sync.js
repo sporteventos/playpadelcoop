66e890c1ccb3daf251d493269937a5945ddace11
euamusse <euamusse@users.noreply.github.com>
2026-06-08 04:54:11 +0200

    fix: incluir config no payload do getAllData (github-sync)

--- js/github-sync.js
+++ js/github-sync.js
@@ -34,6 +34,7 @@ const GHSync = (() => {
       jogos:      getData('jogos'),
       fasefinal:  getData('fasefinal'),
       telefones:  getData('telefones'),
+      config:     getData('config'),
       users:      Auth.getUsers(),
       auditlog:   Auth.getLogs(),
       _updated:   new Date().toISOString()
